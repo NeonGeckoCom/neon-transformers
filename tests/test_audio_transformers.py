acdaaf812a8e34f7cdbff998c1e10f9fd6bbe97d
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2022-06-03 15:05:37 -0700

    Minor changes to prep release (#7)
    * Update license
    Fix typos in automations
    Stub audio transformer tests and add automation
    
    * Stub audio transformer test case
    
    Co-authored-by: Daniel McKnight <daniel@neon.ai>

---
+++ tests/test_audio_transformers.py
@@ -0,0 +1,70 @@
+# NEON AI (TM) SOFTWARE, Software Development Kit & Application Framework
+# All trademark and other rights reserved by their respective owners
+# Copyright 2008-2022 Neongecko.com Inc.
+# Contributors: Daniel McKnight, Guy Daniels, Elon Gasper, Richard Leeds,
+# Regina Bloomstine, Casimiro Ferreira, Andrii Pernatii, Kirill Hrymailo
+# BSD-3 License
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS;  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import os
+import sys
+import unittest
+
+from ovos_utils.messagebus import FakeBus
+
+from neon_transformers.tasks import UtteranceTask
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from neon_transformers import AudioTransformer
+
+
+class MockTransformer(AudioTransformer):
+    task = UtteranceTask.TRANSFORM
+
+    def __init__(self):
+        super().__init__("mock_transformer")
+
+    def transform(self, utterances, context=None):
+        return utterances + ["transformer"], {}
+
+
+class MockContextAdder(AudioTransformer):
+    task = UtteranceTask.ADD_CONTEXT
+
+    def __init__(self):
+        super().__init__("mock_context_adder")
+
+    def transform(self, utterances, context=None):
+        return utterances, {"old_context": False,
+                            "new_context": True,
+                            "new_key": "test"}
+
+
+class AudioTransformersTests(unittest.TestCase):
+    def test_audio_transformer(self):
+        # TODO: Write these tests
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
